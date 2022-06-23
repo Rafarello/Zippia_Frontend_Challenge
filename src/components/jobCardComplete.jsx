bcb407c985d8677ba3992b1f3a3bfe63387a7344
Rafael Moraes <rafael1moraes1rocha@gmail.com>
2022-06-23 12:58:29 -0300

    Feat: Component - JobCardComplete

---
+++ src/components/jobCardComplete.jsx
@@ -0,0 +1,55 @@
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import Parser from 'html-react-parser';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/jobCard.css';
+import { Card, Button } from 'react-bootstrap';
+import storage from '../context/context';
+
+function JobCardComplete() {
+  const { selectedJob } = useContext(storage);
+  const {
+    jobDescription,
+    jobId,
+    jobTitle,
+    companyName,
+  } = selectedJob;
+
+  const descriptionParsed = Parser(String(jobDescription));
+  return (
+    <Card id={jobId} className="card">
+      <Card.Body>
+        <Card.Title>
+          {jobTitle}
+        </Card.Title>
+        <Card.Subtitle>
+          {companyName}
+        </Card.Subtitle>
+        <Card.Text className="job-description-complete">
+          {(descriptionParsed)}
+        </Card.Text>
+        <Button>Show more</Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+JobCardComplete.propTypes = {
+  job: PropTypes.shape({
+    jobId: PropTypes.string,
+    jobTitle: PropTypes.string,
+    companyName: PropTypes.string,
+    jobDescription: PropTypes.string,
+  }),
+};
+
+JobCardComplete.defaultProps = {
+  job: {
+    jobId: '',
+    jobTitle: '',
+    companyName: '',
+    jobDescription: '',
+  },
+};
+
+export default JobCardComplete;
