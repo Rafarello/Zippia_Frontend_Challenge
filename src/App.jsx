f0837b6a71fdae426e1a029739d8a6c6f69bf98d
Rafael Moraes <rafael1moraes1rocha@gmail.com>
2022-06-23 13:05:24 -0300

    Feat: Provider - Provider for children elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
+import Provider from './context/provider';
 import ZippiaPage from './pages/ZippiaPage';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/test/jobs" />} />
-      <Route path="/test/jobs" element={<ZippiaPage />} />
-    </Routes>
+    <Provider>
+      <Routes>
+        <Route path="/" element={<Navigate to="/test/jobs" />} />
+        <Route path="/test/jobs" element={<ZippiaPage />} />
+      </Routes>
+    </Provider>
   );
 }
 
